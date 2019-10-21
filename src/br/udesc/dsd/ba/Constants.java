e74b544fcc3e63affe5ab088818d1b4bb93fb166
Mario <mariofronza1@gmail.com>
2019-10-21 07:44:58 -0300

    fix: Ips list

--- src/br/udesc/dsd/ba/Constants.java
+++ src/br/udesc/dsd/ba/Constants.java
@@ -3,7 +3,7 @@ package br.udesc.dsd.ba;
 public class Constants {
 
     public static int ports[] = {56000, 56001, 56002, 56003, 56004};
-    public static int ips[] = {56000, 56001, 56002, 56003, 56004};
+    public static String ips[] = {"10.60.185.67", "10.60.185.52", "10.60.185.63", "10.60.185.53", "10.60.185.62"};
 
     public static String ELECTION_MESSAGE = "ELECTION";
     public static String OK = "OK";
